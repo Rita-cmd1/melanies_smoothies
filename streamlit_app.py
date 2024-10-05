ad8415882871c7f10ff9dd66812c3d71f1d0742c
Rita-cmd1 <rita.v.gandham@kipi.ai>
2024-10-05 19:45:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,76 @@
-# Import python packages
 import streamlit as st
 from snowflake.snowpark.context import get_active_session
 streamlit.title('My Parents New Healthy Diner')
 # Write directly to the app
-st.title(":cup_with_straw:Customize Your Smoothie! :cup_with_straw:")
+st.title("Example Streamlit App :balloon:")
 st.write(
-"""Choose the fruit you want in your custom Smoothie!
-""")
+    """Replace this example with your own code!
+    **And if you're new to Streamlit,** check
+    out our easy-to-follow guides at
+    [docs.streamlit.io](https://docs.streamlit.io).
+    """
+)
 
-from snowflake.snowpark.functions import col
+# Get the current credentials
+session = get_active_session()
+
+# Use an interactive slider to get user input
+hifives_val = st.slider(
+    "Number of high-fives in Q3",
+    min_value=0,
+    max_value=90,
+    value=60,
+    help="Use this to enter the number of high-fives you gave in Q3",
+)
+
+#  Create an example dataframe
+#  Note: this is just some dummy data, but you can easily connect to your Snowflake data
+#  It is also possible to query data using raw SQL using session.sql() e.g. session.sql("select * from table")
+created_dataframe = session.create_dataframe(
+    [[50, 25, "Q1"], [20, 35, "Q2"], [hifives_val, 30, "Q3"]],
+    schema=["HIGH_FIVES", "FIST_BUMPS", "QUARTER"],
+)
+
+# Execute the query and convert it into a Pandas dataframe
+queried_data = created_dataframe.to_pandas()
+
+# Create a simple bar chart
+# See docs.streamlit.io for more types of charts
+st.subheader("Number of high-fives")
+st.bar_chart(data=queried_data, x="QUARTER", y="HIGH_FIVES")
+
+st.subheader("Underlying data")
+st.dataframe(queried_data, use_container_width=True)
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col, when_matched
+
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+"""Orders that need to be filled"""
+)
 
 session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
-
-ingredients_list = st.multiselect(
-    'Choose up to 5  ingredients:'
-    , my_dataframe
-    )
-
-if ingredients_list:
-     ingredients_string = ''
-
-     for fruit_chosen in ingredients_list:
-         ingredients_string += fruit_chosen
-
-     #st.write(ingredients_string)
-
-
-     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
-
-     #st.write(my_insert_stmt)
-     time_to_insert = st.button('Submit Order')
-
-     if time_to_insert:
-         session.sql(my_insert_stmt).collect()
+my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0).collect()
+if my_dataframe:
+    editable_df = st.data_editor(my_dataframe)
+    submitted=st.button('Submit')
+    if submitted:
     
-         st.success('Your Smoothie is ordered!', icon="✅")
+        og_dataset = session.table("smoothies.public.orders")
+        edited_dataset = session.create_dataframe(editable_df)
+        try:
+            og_dataset.merge(edited_dataset
+                            , (og_dataset['ORDER_UID'] == edited_dataset['name_on_order'])
+                            , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
+                        )
+            st.success('Someone clicked the button', icon = '👍')
+        except:
+            st.write("Something went wrong")
+else:
+    st.success('There are no pending orders right now', icon = '👍')
 
 
